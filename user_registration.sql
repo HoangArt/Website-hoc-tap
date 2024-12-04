18199c0c69caf764eb9dcd280c813c876fee8a68
HoangArt <daylamotcaiemailok@gmail.com>
2024-12-04 19:16:12 +0700

    UPDATE CSDL

--- user_registration.sql
+++ user_registration.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2024 at 01:32 PM
+-- Generation Time: Dec 04, 2024 at 01:15 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -50,18 +50,21 @@ INSERT INTO `roles` (`id`, `role_name`) VALUES
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `full_name` varchar(255) NOT NULL,
-  `contact_info` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role_id` int(11) DEFAULT NULL,
-  `date_of_birth` date NOT NULL
+  `date_of_birth` date NOT NULL,
+  `reset_token_hash` varchar(64) DEFAULT NULL,
+  `reset_token_expires_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `full_name`, `contact_info`, `password`, `role_id`, `date_of_birth`) VALUES
-(1, 'Đẹp Zai', 'admin@hello.com', '$2y$10$.AlN6TmniJKWGm5RH13bIuluitJFVrlxmEbFXVi2ZG7xUZ4psQNqO', 1, '2000-01-01');
+INSERT INTO `users` (`id`, `full_name`, `email`, `password`, `role_id`, `date_of_birth`, `reset_token_hash`, `reset_token_expires_at`) VALUES
+(1, 'Đẹp Zai', 'admin@hello.com', '$2y$10$.AlN6TmniJKWGm5RH13bIuluitJFVrlxmEbFXVi2ZG7xUZ4psQNqO', 1, '2000-01-01', '97a83c219a39baa78071237dcb4937d154814d7900a58fdfcc4acabd59e547ff', '2024-12-03 13:50:23'),
+(3, 'Hello Hello', 'daylamotcaiemailok@gmail.com', '$2y$10$vnW/Z5A9bezobROghkeaTehPU1Qf3b/kEd2DgYbZyWTlfhIYVKevm', 1, '2005-01-01', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -79,7 +82,8 @@ ALTER TABLE `roles`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `contact_info` (`contact_info`),
+  ADD UNIQUE KEY `contact_info` (`email`),
+  ADD UNIQUE KEY `reset_token_hash` (`reset_token_hash`),
   ADD KEY `role_id` (`role_id`);
 
 --
@@ -96,7 +100,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
